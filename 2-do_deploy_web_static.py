58a96d26f717e8eacce112e3af4a19c27e848e41
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-10-31 16:33:43 -0700

    Attempting deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["52.91.121.146", "3.85.136.181"]
 
 
 def do_deploy(archive_path):
